a83c921aa568812f09b87d3ec0985ad18a09d80a
John Crepezzi <john.crepezzi@gmail.com>
2013-11-16 13:36:52 -0500

    Remove bundler/setup from lib

--- lib/haste.rb
+++ lib/haste.rb
@@ -1,4 +1,3 @@
-require 'bundler/setup'
 require File.dirname(__FILE__) + '/haste/uploader'
 require File.dirname(__FILE__) + '/haste/exception'
 require File.dirname(__FILE__) + '/haste/cli'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,2 @@
+require 'bundler/setup'
 require File.dirname(__FILE__) + '/../lib/haste'
